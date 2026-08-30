fd564ec38bb671233b57c208dbf1e2c3a83ea616
aryadanasaputra <arya.dana.saputra01@gmail.com>
2026-08-30 21:01:14 +0700

    add skill-effect (coba)

--- systems/effects/effect.py
+++ systems/effects/effect.py
@@ -1,12 +1,14 @@
 class StatusEffect:
-    def __init__(self, name, duration, damage=0, defense_bonus=0, attack_bonus=0, max_mana_bonus=0, max_health_bonus=0, ):
+    def __init__(self, name, duration, damage=0, defense_bonus=0, attack_bonus=0, health_bonus=0, mana_bonus=0, max_health_bonus=0, max_mana_bonus=0):
         self.name = name
         self.duration = duration
         self.damage = damage
         self.defense_bonus = defense_bonus
         self.attack_bonus = attack_bonus
-        self.max_mana_bonus = max_mana_bonus
+        self.health_bonus = health_bonus
+        self.mana_bonus = mana_bonus
         self.max_health_bonus = max_health_bonus
+        self.max_mana_bonus = max_mana_bonus
 
     def copy(self):
         return StatusEffect(
@@ -15,6 +17,8 @@ class StatusEffect:
             self.damage,
             self.defense_bonus,
             self.attack_bonus,
+            self.health_bonus,
+            self.mana_bonus,
             self.max_health_bonus,
             self.max_mana_bonus
         )
@@ -23,3 +27,4 @@ BUFF = StatusEffect("Buff", duration=3, attack_bonus=5, max_mana_bonus=100)
 WEAKNES = StatusEffect("Weaknes", duration=2, attack_bonus=-2, defense_bonus=-3, max_mana_bonus=-30)
 BERSERK = StatusEffect("Berserk", duration=2, attack_bonus=8, defense_bonus=-4)
 BURN = StatusEffect("Burning", duration=3, damage=3, defense_bonus=-3)
+HEAL = StatusEffect("Heal", duration=2, health_bonus=5)
--- systems/skills/mage.py
+++ systems/skills/mage.py
@@ -1,5 +1,5 @@
 from .skill import Skill
-from systems.effects.effect import BURN
+from systems.effects.effect import BURN, HEAL
 
 IGNITE = Skill(
     "Ignite",
@@ -15,4 +15,10 @@ IGNITE = Skill(
     damage=10,
     effect=BURN,
     effect_target="enemy"
+)
+
+HEALING = Skill(
+    "Healing",
+    mana_cost=15,
+    effect=HEAL
 )
